7a9f38d071b602ea7734595ae239769c75cb7512
Marco Cárdenas <120759277+Marco-Cardenas@users.noreply.github.com>
2025-06-29 10:14:50 -0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CrudModule } from './crud/crud.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/backend-prueba'), CrudModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://landingPageBD:landingPageBD2025Web@cluster0.7wc3n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Prueba_Tecnica'), CrudModule],
   controllers: [AppController],
   providers: [AppService],
 })
